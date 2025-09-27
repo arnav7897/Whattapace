6420ffaa434f959acd915ab33f24bf91b044af60
arnav7897 <asharda7898@gmail.com>
2025-09-27 17:40:48 +0530

    7th commit

--- Frontend/src/Components/NavBar.tsx
+++ Frontend/src/Components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import type { FC } from "react";
 import { Menu, X, ArrowUpRight } from "lucide-react";
 
